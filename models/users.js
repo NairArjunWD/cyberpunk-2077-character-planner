b44c01b1f56d703cd91b2829962a63901681928f
Arjun Nair <nair.arjun47@yahoo.com>
2019-10-31 15:55:04 -0700

    login works

--- models/users.js
+++ models/users.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     username: String,
     // email: String,
     password: String
 });
+
+userSchema.methods.checkPassword = function (guess, done) {
+    console.log(guess)
+    bcrypt.compare(guess, this.password, function (err, isMatch) {
+        done(err, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', userSchema);
--- setuppassport.js
+++ setuppassport.js
@@ -17,26 +17,26 @@ passport.use(new LocalStrategy(
     }
 ));
 
-// passport.use('login', new LocalStrategy(
-//     function(username, password, done) {
-//          User.findOne({username: username}, function(err, user) {
-//              if (err) {return done(err);}
-//              if(!user) {
-//                  return done(null, false,
-//                     {message: 'No user has that username!'});
-//              }
-//              user.checkPassport(passport, function(err, isMatch){
-//                  if (err) {return done (err); }
-//                  if (isMatch) {
-//                      return done (null, user);
-//                  } else {
-//                      return done (null, false,
-//                         {message: 'Invalid password'});
-//                  }
-//              });
-//          });
-//     }
-// ));
+passport.use('/login', new LocalStrategy(
+    function(username, password, done) {
+         User.findOne({username: username}, function(err, user) {
+             if (err) {return done(err);}
+             if(!user) {
+                 return done(null, false,
+                    {message: 'No user has that username!'});
+             }
+             user.checkPassword(password, function(err, isMatch){
+                 if (err) {return done (err); }
+                 if (isMatch) {
+                     return done (null, user);
+                 } else {
+                     return done (null, false,
+                        {message: 'Invalid password'});
+                 }
+             });
+         });
+    }
+));
 
 module.exports = function() {
     passport.serializeUser(function(user, done) {
